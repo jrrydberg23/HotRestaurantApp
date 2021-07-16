d31fbed5cc82a981d03b50d763e42096f5cc798a
Truong Duong <truong.duong1908@gmail.com>
2021-07-15 19:25:23 -0500

    add few variables

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const path = require('path');
+const { getMaxListeners } = require('process');
 
 // Sets up the Express App
 
 const app = express();
 const PORT = 3000;
 
-
+const reservation = [
+    {
+        table: 1,
+        name: 'Yoda',
+        phoneNumber: 469-469-4690,
+        Email: "yoda@gmail.com",
+        uniqueId: ydacoolas151,
+    },
+    {
+        table: 2,
+        name: 'chicken',
+        phoneNumber: 469-314-4500,
+        Email: "chickenemail@gmail.com",
+        uniqueId: chicken41,
+    },
+    {
+        table: 3,
+        name: 'monkey',
+        phoneNumber: 469-220-4580,
+        Email: "moneky@gmail.com",
+        uniqueId: monkey51,
+    },
+  ];
 
 
 
